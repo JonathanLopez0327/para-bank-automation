07b9db05627438888e0956a229f498a14af1d98a
Jonathan Lopez <jonathan_lopez@bhd.com.do>
2025-01-14 23:39:31 -0400

    Workflow fixed

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var cypress_1 = require("cypress");
 exports.default = (0, cypress_1.defineConfig)({
     e2e: {
-        specPattern: 'cypress/e2e/test/**/*.cy.{js,ts}',
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/e2e.ts',
         setupNodeEvents: function (on, config) {
             // implement node event listeners here
         },
