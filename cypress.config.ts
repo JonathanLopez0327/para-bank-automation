4742dc04572411a86abf2b149bc5d08936d46695
Jonathan Lopez <jonathan_lopez@bhd.com.do>
2025-01-14 23:30:26 -0400

    Workflow fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/test/**/*.cy.{js,ts}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
